83471d5903cbfdb71088fbd37791139a962512ce
Brian Voskerijian <Brian Voskerijian>
2017-03-26 12:15:51 -0400

    Add routing

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,7 +4,6 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
-    selector: 'pm-products',
     templateUrl: 'product-list.component.html',
     styleUrls: ['product-list.component.css']
 })
